cbfcef32b0663f1e0d59051b666fd70f33639a12
Philipp Mallot <philipp.mallot@fridata.de>
2019-11-25 16:59:59 +0100

    spinner is way less annoying than the bar

--- app.R
+++ app.R
@@ -36,7 +36,7 @@ ui <- tagList(
   useShinyjs(),
   includeCSS("style.css"),
   extendShinyjs("script.js"),
-  add_busy_bar(color = "#0a8043", height = "5px", centered=T),
+  add_busy_spinner(spin="circle"),
   navbarPage(id = "navbar", "rteu-logger-app v1.3",
     source("ui/uiTabData.R")$value,
     source("ui/uiTabLive.R")$value,
