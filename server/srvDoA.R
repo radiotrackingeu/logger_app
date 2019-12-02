0f36937d6b62fcbedbb972bf78e6b71d4b2c48ca
Philipp Mallot <philipp.mallot@fridata.de>
2019-12-02 13:35:14 +0100

    add ignore_back case to old doa_method doa_internal however does not create tags yet

--- server/srvDoA.R
+++ server/srvDoA.R
@@ -189,9 +189,11 @@ doa_internal <- function(data, time_to_look_for, dBLoss=14, doa_approx="automati
           }else{
             #back antenna plays a big role here
             if(nrow(data_tfs)>2){
-              angle_1<-data_tfs[1,"Orientation"]
-              angle_2<-calc_angle(data_tfs[1,"max_signal"],data_tfs[3,"max_signal"],data_tfs[1,"Orientation"],data_tfs[3,"Orientation"],2*dBLoss,"linear")
-              angle<-angle_1+angle_between(angle_1,angle_2)/abs(angle_between(data_tfs[1,"Orientation"],data_tfs[2,"Orientation"]))*60
+              num_angle_2<-2
+              if ((data_tfs[1,"Orientation"]+180)%%360==data_tfs[2,"Orientation"]){
+                num_angle_2<-3
+              }
+              angle<-calc_angle(data_tfs[1,"max_signal"],data_tfs[num_angle_2,"max_signal"],data_tfs[1,"Orientation"],data_tfs[num_angle_2,"Orientation"],1.5*dBLoss,"linear")
               result<-cbind.data.frame(timestamp=as.POSIXct(t,origin="1970-01-01",tz="UTC"),angle=angle,antennas=nrow(data_tfs),Station=s,freq_tag=f,strength=max(data_tfs$max_signal),stringsAsFactors=F)
             }
             if(nrow(data_tfs)==2&use_back_antenna){
