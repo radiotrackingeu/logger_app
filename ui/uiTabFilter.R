4f869f5d4270d4da341df8ba4671fd104a4fa897
Philipp Mallot <philipp.mallot@fridata.de>
2021-11-04 13:12:59 +0100

    make (some) plots full height

--- ui/uiTabFilter.R
+++ ui/uiTabFilter.R
@@ -1,76 +1,119 @@
 ############ tabFilter.R ############
 tabPanel("Filter",
-         fluidRow(
-           column(9,
-                  column(4,
-                         checkboxInput("filter_length",strong("Signal length"),value = FALSE),
-                         sliderInput("signal_length",
-                                     "",
-                                     min = 0.0005,
-                                     max = 0.050,
-                                     value = c(0.01,0.03))
-                  ),
-                  column(4,
-                         checkboxInput("filter_strength",strong("Signal Strength"),value = FALSE),
-                         sliderInput("signal_strength",
-                                     "",
-                                     min = 0,
-                                     max = 100,
-                                     value = c(15,95))
-                  ),
-                  column(4,
-                         checkboxInput("filter_bw",strong("Bandwidth"),value = FALSE),
-                         sliderInput("signal_bw",
-                                     "",
-                                     min = 500,
-                                     max = 50000,
-                                     value = c(2000,8000))
-                  ),
-                  tabsetPanel(type = "tabs",
-                              tabPanel("Frequency",
-                                       plotOutput("histo",hover = "plot_freq_hover")
-                              ),
-                              tabPanel("Duration",
-                                       plotOutput("histo_length")
-                              ),
-                              tabPanel("Signal Strength",
-                                       plotOutput("histo_strength")
-                              ),
-                              tabPanel("Signal Bandwidth",
-                                       plotOutput("histo_bandwidth")
-                              )
-                  )
-           ),
-           column(3,
-                  dateRangeInput("filter_for_dates","Choose dates"),
-                  radioButtons("filter_type", strong("Filter type:"), c("all","Multiple frequencies", "Custom frequency")),
-                  conditionalPanel(condition='input.filter_type == "Custom frequency"',
-                                   numericInput("single_freq", "Enter a frequency", value = 150175),
-                                   tags$span(style="font-weight:bold","For temperature curve a*e^b:"),
-                                   splitLayout(
-                                     numericInput("single_freq_temp_a", "Coefficient a",value = 20.307),
-                                     numericInput("single_freq_temp_b", "Coefficient b",value = 0.0408)
-                                   )
-                  ),
-                  conditionalPanel(condition='input.filter_type == "Multiple frequencies"',
-                                   uiOutput("freq_tags")
-                  ),
-                  sliderInput("freq_error",
-                              "Frequency Error (kHz):",
-                              min = 0.1,
-                              max = 30,
-                              value = 2,
-                              step=0.1),
-                  checkboxInput("filter_interval",strong("Signal Interval Filter"),value = FALSE),
-                  sliderInput("signal_interval",
-                              "Signal Period in sec:",
-                              min = 0.5,
-                              max = 8,
-                              value = c(0.8,1.2)),
-                  selectInput("input_select_receiver", "Select Receiver/s", choices = NULL, multiple = TRUE, selectize = TRUE),
-                  selectInput("input_select_station", "Select Station/s", choices = NULL, multiple = TRUE, selectize = TRUE),
-                  textOutput("total_counts"),
-                  h4(textOutput("freq_hover"))
-           )
-         )
-)
+  fluidRow(
+    column(
+      9,
+      column(
+        4,
+        checkboxInput("filter_length", strong("Signal length"), value = FALSE),
+        sliderInput(
+          "signal_length",
+          "",
+          min = 0.0005,
+          max = 0.050,
+          value = c(0.01, 0.03)
+        )
+      ),
+      column(
+        4,
+        checkboxInput("filter_strength", strong("Signal Strength"), value = FALSE),
+        sliderInput(
+          "signal_strength",
+          "",
+          min = 0,
+          max = 100,
+          value = c(15, 95)
+        )
+      ),
+      column(
+        4,
+        checkboxInput("filter_bw", strong("Bandwidth"), value = FALSE),
+        sliderInput(
+          "signal_bw",
+          "",
+          min = 500,
+          max = 50000,
+          value = c(2000, 8000)
+        )
+      ),
+
+      tabsetPanel(
+        type = "tabs",
+        tabPanel("Frequency",
+          div(
+            style="height: calc(100vh - 280px)",
+            plotOutput("histo", hover = "plot_freq_hover", height="100%"))
+        ),
+        tabPanel("Duration",
+          div(
+            style="height: calc(100vh - 280px)",
+            plotOutput("histo_length", height="100%")
+          )
+        ),
+        tabPanel("Signal Strength",
+          div(
+            style="height: calc(100vh - 280px)",
+            plotOutput("histo_strength", height="100%")
+          )
+        ),
+        tabPanel("Signal Bandwidth",
+          div(
+            style="height: calc(100vh - 280px)",
+            plotOutput("histo_bandwidth", height="100%")
+          )
+        )
+      )
+    ),
+    column(
+      3,
+      dateRangeInput("filter_for_dates", "Choose dates"),
+      radioButtons(
+        "filter_type",
+        strong("Filter type:"),
+        c("all", "Multiple frequencies", "Custom frequency")
+      ),
+      conditionalPanel(
+        condition = 'input.filter_type == "Custom frequency"',
+        numericInput("single_freq", "Enter a frequency", value = 150175),
+        tags$span(style = "font-weight:bold", "For temperature curve a*e^b:"),
+        splitLayout(
+          numericInput("single_freq_temp_a", "Coefficient a", value = 20.307),
+          numericInput("single_freq_temp_b", "Coefficient b", value = 0.0408)
+        )
+      ),
+      conditionalPanel(condition = 'input.filter_type == "Multiple frequencies"',
+        uiOutput("freq_tags")),
+      sliderInput(
+        "freq_error",
+        "Frequency Error (kHz):",
+        min = 0.1,
+        max = 30,
+        value = 2,
+        step = 0.1
+      ),
+      checkboxInput("filter_interval", strong("Signal Interval Filter"), value = FALSE),
+      sliderInput(
+        "signal_interval",
+        "Signal Period in sec:",
+        min = 0.5,
+        max = 8,
+        value = c(0.8, 1.2)
+      ),
+      selectInput(
+        "input_select_receiver",
+        "Select Receiver/s",
+        choices = NULL,
+        multiple = TRUE,
+        selectize = TRUE
+      ),
+      selectInput(
+        "input_select_station",
+        "Select Station/s",
+        choices = NULL,
+        multiple = TRUE,
+        selectize = TRUE
+      ),
+      textOutput("total_counts"),
+      h4(textOutput("freq_hover"))
+    )
+  ))
--- ui/uiTabResults.R
+++ ui/uiTabResults.R
@@ -1,40 +1,55 @@
 ############ tabFilters.R ############
 tabPanel("Results",
-         fluidRow(
-           column(3,selectInput("choose_plot","Choose Plot",
-                                choices = c("Time-Strength-Receiver-Station",
-                                            "Time-Strength-Receiver-Station-Freq",
-                                            "Time-Freq-Strength-Station",
-                                            "Time-Strength-Frequency-Station",
-                                            "Time-Temperature-Station-Frequency",
-                                            "Time-TD-Station-Frequency"
-                                ),
-                                selected="Time-Strength-Receiver-Station")
-                  ),
-           column(3,
-                  actionButton("minus_one_day","Minus one day"),
-                  actionButton("plus_one_day","Plus one day")
-                  ),
-           column(3,offset = 3,
-                  br(),
-                  h4(textOutput("plot_x_y"))
-                  )
-         ),
-                plotOutput("facet",hover = "plot_hover"),
-         fluidRow(
-           column(10,offset = 1,
-                  sliderInput("slider_datetime", "",
-                              min=as.POSIXlt("2017-09-24 00:00:00", "UTC"),
-                              max=as.POSIXlt("2017-09-29 23:59:59", "UTC"),
-                              value=c(as.POSIXlt("2017-09-24 00:00:00", "UTC"),
-                                      as.POSIXlt("2017-09-29 23:59:59", "UTC")
-                              ),
-                              timezone = "UTC",
-                              dragRange = TRUE,
-                              animate = TRUE,
-                              width = "100%",
-                              timeFormat="%d-%m %H:%M:%S")
-           )
-         )
+  fluidRow(
+    column(
+      width=3,
+      selectInput("choose_plot","Choose Plot",
+        choices = c("Time-Strength-Receiver-Station",
+                    "Time-Strength-Receiver-Station-Freq",
+                    "Time-Freq-Strength-Station",
+                    "Time-Strength-Frequency-Station",
+                    "Time-Temperature-Station-Frequency",
+                    "Time-TD-Station-Frequency"
+        ),
+        selected="Time-Strength-Receiver-Station"
+      )
+    ),
+    column(
+      width=3,
+      actionButton("minus_one_day","Minus one day"),
+      actionButton("plus_one_day","Plus one day")
+    ),
+    column(
+      width=3,
+      offset = 3,
+      br(),
+      h4(textOutput("plot_x_y"))
+    )
+  ),
+  div(
+    plotOutput("facet",hover = "plot_hover", height="100%"),
+    style="height: calc(100vh - 260px)"
+  ),
+  fluidRow(
+    column(
+      width=10,
+      offset = 1,
+      sliderInput(
+        "slider_datetime", 
+        "",
+        min=as.POSIXlt("2017-09-24 00:00:00", "UTC"),
+        max=as.POSIXlt("2017-09-29 23:59:59", "UTC"),
+        value=c(
+          as.POSIXlt("2017-09-24 00:00:00", "UTC"),
+          as.POSIXlt("2017-09-29 23:59:59", "UTC")
+        ),
+        timezone = "UTC",
+        dragRange = TRUE,
+        animate = TRUE,
+        width = "100%",
+        timeFormat="%d-%m %H:%M:%S"
+      )
+    )
+  )
 )
 
