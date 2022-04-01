f49ac10e0a2899fda84d74ca3da7266ea4541c6b
Philipp Mallot <philipp.mallot@fridata.de>
2022-04-01 16:39:34 +0200

    allow Time-Freq-Strenght-Station plot without Freq selection

--- server/srvTabResults.R
+++ server/srvTabResults.R
@@ -42,7 +42,7 @@ output$facet <- renderPlot({
          },
          'Time-Freq-Strength-Station'={
            # ggplot(filtered_data()) +
-           get_base_plot(filtered_data_td(), style="white") +
+           get_base_plot(filtered_data(), style="white") +
              geom_point(aes(x=timestamp, y=signal_freq, color=max_signal)) +
              labs(x="Time", y = "Frequency") +
              scale_x_datetime(labels = function(x) format(x, "%d-%m \n %H:%M:%S"))+
