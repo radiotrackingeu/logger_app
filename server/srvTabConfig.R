538f7554c7a02c5a0a69ea09962dc51d5b84dea0
Philipp Mallot <philipp.mallot@plecotus-solutions.de>
2023-04-20 15:21:39 +0200

    intial version of upload

--- server/srvTabConfig.R
+++ server/srvTabConfig.R
@@ -42,3 +42,33 @@ observeEvent(global$config, ignoreInit = T, {
   updateAceEditor(session = session, editorId = "conf_editor", value = global$config)
 })
 
+observeEvent(input$conf_uload, ignoreInit = T, {
+  tfile<-tempfile(pattern=paste0("rteu_", format(Sys.time(), "%FT%Hh%Mm"),"_"), fileext = ".json")
+  fileConn<-file(tfile)
+  writeLines(input$conf_editor, fileConn, sep = "")
+  close(fileConn)
+  
+  recs <- subset(global$connections,Name %in% input$conf_select_connection)
+  
+  cl<-parallel::makeCluster(min(detectCores(),nrow(recs)))
+  doParallel::registerDoParallel(cl)
+  tmp<- foreach(i = iter(recs, by="row"), .export = c("session")) %do% {
+    ret<-tryCatch({
+      RCurl::ftpUpload(
+        what = tfile,
+        to = paste0("sftp://sftp@", i$Host, ":", i$Port+1, "/home/sftp/rteu.json"),
+        verbose = TRUE,
+        .opts = list(
+          ssh.private.keyfile = "./sftp_rsa", 
+          ssh.public.keyfile = "./sftp_rsa.pub"
+        ) 
+      )
+    })
+    if (ret>0) {
+      showNotification(session = session, ui = HTML("Upload to ", i$Name ," failed. Please download to see current state and try again."), type="error")
+    } else {
+      showNotification(session = session, ui = HTML("Upload to ", i$Name ," successful."), type="message")
+    }
+  }
+  parallel::stopCluster(cl)
+})
