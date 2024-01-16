13966d50716f06a899c3b92de0344f5cb0e01b3a
Philipp Mallot <philipp.mallot@plecotus-solutions.de>
2024-01-16 13:27:37 +0100

    fix wrong logical operator

--- server/srvResults.R
+++ server/srvResults.R
@@ -59,9 +59,9 @@ get_base_plot <- function(signal_data, signal_aes=aes(), style="none") {
   require(data.table)
   p<-ggplot(signal_data, signal_aes)
   if(
-    !is.null(global$keepalives) & 
-      nrow(global$keepalives)>0 & 
-      "td_fctr" %in% names(global$keepalives) &
+    !is.null(global$keepalives) && 
+      nrow(global$keepalives)>0 &&
+      "td_fctr" %in% names(global$keepalives) &&
       as.data.table(global$keepalives)[td_fctr=="down" & timestamp %between% c(min(na.rm=T, signal_data$timestamp), max(na.rm=T, signal_data$timestamp)),.N]>0
     ) {
     if(!is.data.table(global$keepalives))
